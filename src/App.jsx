a295ea000bc8c0949d4cc1e26f1204ec1b77d586
AronDevpro <140414272+AronDevpro@users.noreply.github.com>
2024-07-02 17:07:25 +0530

    Basic settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import PageLayout from "./Component/Layout/PageLayout.jsx";
+import HomePage from "./View/HomePage/HomePage.jsx";
+import CustomerPage from "./View/CustomerPage/CustomerPage.jsx";
+import EmployeePage from "./View/EmployeePage/EmployeePage.jsx";
+import MenuPage from "./View/MenuPage/MenuPage.jsx";
+import OrderPage from "./View/OrderPage/OrderPage.jsx";
+import SupplierPage from "./View/SupplierPage/SupplierPage.jsx";
+import InventoryPage from "./View/InventoryPage/InventoryPage.jsx";
+import ReservationPage from "./View/ReservationPage/ReservationPage.jsx";
+import RestaurantPage from "./View/RestaurantPage/RestaurantPage.jsx";
 
 function App() {
-    return (
-        <div></div>
-    );
+    const router = createBrowserRouter([
+        {
+            path: '/',
+            element: <PageLayout />,
+            children: [
+                {
+                    path: '/',
+                    element: <HomePage />,
+                },
+                {
+                    path: '/customer',
+                    element: <CustomerPage />,
+                },
+                {
+                    path: '/employee',
+                    element: <EmployeePage />,
+                },
+                {
+                    path: '/menu',
+                    element: <MenuPage />,
+                },
+                {
+                    path: '/order',
+                    element: <OrderPage />,
+                },
+                {
+                    path: '/supplier',
+                    element: <SupplierPage />,
+                },
+                {
+                    path: '/inventory',
+                    element: <InventoryPage />,
+                },
+                {
+                    path: '/reservation',
+                    element: <ReservationPage />,
+                },
+                {
+                    path: '/restaurant',
+                    element: <RestaurantPage />,
+                },
+            ],
+        },
+    ])
+    return <RouterProvider router={router} />
 }
 
-export default App;
+export default App
---
+++ src/utils/Server.js
@@ -0,0 +1,10 @@
+const localhost = "http://127.0.0.1:5000";
+
+export const customer = `${localhost}/customers`;
+export const employee = `${localhost}/employees`;
+export const reservation = `${localhost}/reservations`;
+export const inventory = `${localhost}/inventories`;
+export const menu = `${localhost}/menus`;
+export const supplier = `${localhost}/suppliers`;
+export const restaurant = `${localhost}/restaurants`;
+export const order = `${localhost}/orders`;
